5d0647f2de30f7257da9660de199558d577d084e
Jean Morais <jeancdemorais@gmail.com>
2018-07-19 19:57:39 -0300

    Change expected payloads

--- test/graylog-rocketchat.hooks.test.js
+++ test/graylog-rocketchat.hooks.test.js
@@ -11,30 +11,33 @@ const getRequest = (filename) => {
     return {request:{content: content}};
 };
 
-
 test('Handles dummy alert', () => {
     const response = new Script().process_incoming_request(getRequest('dummy-alert.json'));
-    expect(response.content.text).toBe('Dummy alert to test notifications');
+    expect(response.content.attachments[0].text).toContain('Dummy alert to test notifications');
 });
 
 test('Handles error alert', () => {
     const response = new Script().process_incoming_request(getRequest('error-alert.json'));
     const expected = [
-        'A global exception on URL http://www.example.com, headers: {Cookie=__cfduid=dd00f87ca1571f030cd86dc107ce0fbee1502342427; language=ro; JSESSIONID=example1rw0aif2vail73dck6kkzx1hk.example1; searches=1; gender=; birthyear=, CF',
+        ':warning: *Alert*: Log level is error', 
+        '*Message*: A global exception on URL http://www.example.com, headers: {Cookie=__cfduid=dd00f87ca1571f030cd86dc107ce0fbee1502342427; language=ro; JSESSIONID=example1rw0aif2vail73dck6kkzx1hk.example1; searches=1; gender=; birthyear=, CF', 
+        '*Source*: www.example.com',
         '[Display complete message](https://graylog.example.com/messages/graylog_7/f7bc4d31-cf47-11e7-84b1-0242ac120004)'
     ];
-    expect(response.content.text.split('\n')).toEqual(expected);
+    expect(response.content.attachments[0].text.split('\n')).toEqual(expected);
 });
 
 test('Aggregates same messages in error', () => {
     const response = new Script().process_incoming_request(getRequest('multimessage-alert.json'));
     const expected = [
-        'java.lang.NullPointerException',
+        ':warning: *Alert*: Errors on www.example.com',
+        '*Message*: java.lang.NullPointerException',
         'URL: http://www.example.com',
+        '*Source*: www.example.com',
         '[Display complete message](https://graylog.example.com/messages/graylog_7/d369a310-d0f5-11e7-84b1-0242ac120004), [#2](https://graylog.example.com/messages/graylog_7/d28117d1-d0f5-11e7-84b1-0242ac120004)'
     ];
 
     console.log(response.content.text);
 
-    expect(response.content.text.split('\n')).toEqual(expected);
+    expect(response.content.attachments[0].text.split('\n')).toEqual(expected);
 });
